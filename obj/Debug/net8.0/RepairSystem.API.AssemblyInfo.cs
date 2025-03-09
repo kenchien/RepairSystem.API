d9a4690831e72947f8e48c4c0615e6f66edb4e92
KenChien <andisand@gmail.com>
2025-03-10 05:46:38 +0800

    test

--- obj/Debug/net8.0/RepairSystem.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/RepairSystem.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepairSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ed75ec2bea4595cf437473e82e7a2dc376695f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb2c247f425c488fac8323e7d3c0fce6fe0f0f69")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepairSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepairSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
